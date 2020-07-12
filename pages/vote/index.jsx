eff04ed02e8b6e95f9cda1d2a04fd03fd2e17ab6
Daniel Waruo <waruodaniel@gmail.com>
2020-07-12 07:21:24 +0300

    removed voting pages

--- pages/vote/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {withApollo} from "../../apollo";
-import Vote from "../../components/vote";
-import {withApp} from "../../components/app";
-
-export default withApollo({ssr:false})(
-  withApp(Vote)
-);
--- pages/vote/results.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {withApollo} from "../../apollo";
-import Results from "../../components/vote/results";
-import {withApp} from "../../components/app";
-
-export default withApollo({ssr:false})(
-  withApp(Results)
-);
