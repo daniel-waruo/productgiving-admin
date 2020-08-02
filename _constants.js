2ac66e1d175795e1ac79dd6dae16dca61ae406b9
Daniel Waruo <waruodaniel@gmail.com>
2020-08-03 00:35:36 +0300

    set production url

--- _constants.js
+++ _constants.js
@@ -6,12 +6,12 @@ export const make_url = (domain, protocol = 'http', secure = false) => {
   return `${reqProtocol}://${domain}`
 }
 export const API_URL = dev ? make_url('127.0.0.1:8000') :
-  make_url('api-voting.herokuapp.com', 'http', true);
+  make_url('m-subscribe-api.herokuapp.com', 'http', true);
 
 export const GRAPHQL_ENDPOINT = `${API_URL}/graph_ql`;
 
 export const API_WS_URL = dev ? make_url('127.0.0.1:8000','ws') :
-  make_url('api-voting.herokuapp.com', 'http', true);
+  make_url('m-subscribe-api.herokuapp.com', 'http', true);
 
 export const GRAPHQL_WS_ENDPOINT = `${API_WS_URL}/ws/graph_ql`
 
