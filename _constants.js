4f937b30dca16a9354e98bd4c1c8cd86eb917dcd
Daniel Waruo <waruodaniel@gmail.com>
2020-08-10 17:32:39 +0300

    cleared unused constants in _constants.js

--- _constants.js
+++ _constants.js
@@ -6,24 +6,13 @@ export const make_url = (domain, protocol = 'http', secure = false) => {
   return `${reqProtocol}://${domain}`
 }
 
-export const API_URL = dev ? make_url('127.0.0.1:8000') :
-  make_url('m-subscribe-api.herokuapp.com', 'http', true);
+export const API_URL = dev ? make_url('127.0.0.1:8000') : make_url('m-subscribe-api.herokuapp.com', 'http', true);
 
 export const GRAPHQL_ENDPOINT = `${API_URL}/graph_ql`;
 
-export const API_WS_URL = dev ? make_url('127.0.0.1:8000', 'ws') :
-  make_url('m-subscribe-api.herokuapp.com', 'ws', true);
+export const API_WS_URL = dev ? make_url('127.0.0.1:8000', 'ws') : make_url('m-subscribe-api.herokuapp.com', 'ws', true);
 
 export const GRAPHQL_WS_ENDPOINT = `${API_WS_URL}/ws/graph_ql`
-
-/******************************
- * AUTH API ENDPOINTS
- ******************************/
-export const LOGIN_URL = `${API_URL}/accounts/login/`;
-
-export const LOGOUT_URL = `${API_URL}/accounts/logout/`;
-
-export const REGISTER_URL = `${API_URL}/accounts/registration/`;
 
 /******************************
  * SOCIAL LOGIN CLIENT IDS
@@ -35,5 +24,3 @@ export const GOOGLE_CONFIG = {
          'https://www.googleapis.com/auth/userinfo.profile ' +
          'https://www.googleapis.com/auth/userinfo.email '
 };
-
-export const GOOGLE_LOGIN_URL = `${API_URL}/accounts/social/google/`;
